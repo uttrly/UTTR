2a23be7c2457cfdc91572ba39e59329e5135cd66
Sarah Sakhri <sarahsakhri@Sarahs-MacBook-Pro.local>
2019-05-12 11:20:45 -0400

    user authentication added

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,15 +4,6 @@ module.exports = function (app) {
   app.get("/", function (req, res) {
     res.render("home");
   });
-  app.get("/login", function (req, res) {
-    res.render("login");
-  });
-  app.get("/signup", function (req, res) {
-    res.render("signup");
-  });
-  app.get("/dashboard", function (req, res) {
-    res.render("dashboard");
-  });
   app.get("/createGoal", function (req, res) {
     res.render("createGoal");
   });
@@ -20,6 +11,6 @@ module.exports = function (app) {
     res.render("challenge");
   });
   app.get("*", function (req, res) {
-    res.render("404page");
+    res.render("404");
   });
 }
