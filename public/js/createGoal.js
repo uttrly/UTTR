ab4a99d6a35f44e969f846a2a4949447c4ff4126
SharonC <sharon.s.c.chien@gmail.com>
2019-05-12 22:55:19 -0400

    added some comments

--- public/js/createGoal.js
+++ public/js/createGoal.js
@@ -50,6 +50,7 @@ $(document).ready(function () {
 
   }
 
+  //random keygen for file name
   function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
